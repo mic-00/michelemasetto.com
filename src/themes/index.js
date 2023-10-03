8401a954a9247d22041688021f8c8ac01afc03db
mic-00 <michele.masetto@gmail.com>
2023-10-03 21:43:20 +0200

    refactoring

---
+++ src/themes/index.js
@@ -0,0 +1,11 @@
+import christmasTheme from "./christmas.js";
+import darkTheme from "./dark.js";
+import lightTheme from "./light.js";
+
+const themes = {
+  christmasTheme,
+  darkTheme,
+  lightTheme
+};
+
+export default themes;
