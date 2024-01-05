fcc7870c93765c897d57a527ec94e621e0342dde
mic-00 <michele.masetto@gmail.com>
2024-01-05 16:40:14 +0100

    feat(layout): add skeleton to WorkExperiences component
    Show before data gets loaded.

--- src/components/sections/WorkExperiences/WorkExperiences.jsx
+++ src/components/sections/WorkExperiences/WorkExperiences.jsx
@@ -3,7 +3,7 @@ import {CalendarMonth, Place, Work} from "@mui/icons-material";
 import TimelineOppositeContent, {timelineOppositeContentClasses} from "@mui/lab/TimelineOppositeContent";
 import {timelineItemClasses} from "@mui/lab/TimelineItem";
 import {Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator} from "@mui/lab";
-import {List, ListItem, ListItemIcon, ListItemText, Typography} from "@mui/material";
+import {List, ListItem, ListItemIcon, ListItemText, Skeleton, Typography} from "@mui/material";
 import {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {firestore} from "firebase.js";
@@ -33,7 +33,37 @@ function WorkExperiences() {
           flexGrow: 1
         }
       }}>
-        {workExperiences.map((it, key) => (
+        {workExperiences.length === 0 && (
+            <>
+              <TimelineItem>
+                <TimelineOppositeContent></TimelineOppositeContent>
+                <TimelineSeparator>
+                    <TimelineDot color="primary">
+                      <Work fontSize="small" />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Skeleton variant="text" height={60} />
+                    <Skeleton variant="rectangular" height={100} />
+                  </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineOppositeContent></TimelineOppositeContent>
+                <TimelineSeparator>
+                    <TimelineDot color="primary">
+                      <Work fontSize="small" />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <Skeleton variant="text" height={60} />
+                    <Skeleton variant="rectangular" height={100} />
+                  </TimelineContent>
+              </TimelineItem>
+            </>
+        )}
+        {workExperiences.length > 0 && workExperiences.map((it, key) => (
             <TimelineItem key={key}>
               <TimelineOppositeContent></TimelineOppositeContent>
               <TimelineSeparator>
